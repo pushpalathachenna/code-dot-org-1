ab5923237c48a0870f73e9422ded3a7cc01a147c
Will Jordan <will@code.org>
2020-09-14 13:23:05 -0700

    WIP ssm session manager permissions and Slack integration [ci skip]

--- lib/cdo/cloud_formation/vpc.rb
+++ lib/cdo/cloud_formation/vpc.rb
@@ -2,7 +2,6 @@ module Cdo::CloudFormation
   # Helper functions related to VPC subnets.
   module VPC
     AVAILABILITY_ZONES = ('b'..'e').map {|i| "us-east-1#{i}"}
-    SSH_IP = '0.0.0.0/0'.freeze
 
     def azs(zones=AVAILABILITY_ZONES)
       zones.map {|zone| zone[-1].upcase}
